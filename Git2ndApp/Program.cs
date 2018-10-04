8eefc0604665ae178df2bdd64c13329de47d55f8
uiswtlds <italo.desio@fastwebnet.it>
2018-10-04 12:41:33 +0200

    Add project files.

---
+++ Git2ndApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git2ndApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
